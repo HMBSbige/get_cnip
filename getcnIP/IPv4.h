5b5073f57a8f5164f9b8b3ebe76fc89cb22691f1
HMBSbige <admin@bige0.com>
2017-09-27 19:50:05 +0800

    增加注释;优化

--- getcnIP/IPv4.h
+++ getcnIP/IPv4.h
@@ -7,19 +7,19 @@ private:
 	std::vector<ll> n_;
 public:
 	ipv4(const ipv4& a);
-	explicit ipv4(ll n1 = 0, ll n2 = 0, ll n3 = 0, ll n4 = 0);
-	explicit ipv4(std::string ip);
-	explicit ipv4(std::vector<ll> a);
-	void get_ipv4(std::string ip);
-	ll get_ipv4(size_t i);
-	std::vector<ll> get_ipv4() const;
-	std::string str();
+	explicit ipv4(ll n1 = 0, ll n2 = 0, ll n3 = 0, ll n4 = 0);//用点分十进制初始化
+	explicit ipv4(std::string ip);//用ip字符串初始化，如x.x.x.x
+	explicit ipv4(std::vector<ll> a);//用大小为4的vector初始化
+	void get_ipv4(std::string ip);//
+	ll get_ipv4(size_t i);//返回ipv4第i个十进制数，i=0,1,2,3
+	std::vector<ll> get_ipv4() const;//返回大小为4的vector，4个用小数点分开的十进制数
+	std::string str();//返回ipv4字符串，如x.x.x.x
 };
 struct ip_list
 {
 	ipv4 first_ip;
 	ipv4 last_ip;
-	std::string mask, cn_ipv4;
+	std::string mask;
 	ip_list() {}
 	ip_list(std::string a, std::string b, std::string c) :first_ip(a), last_ip(c), mask(b) {}
 
@@ -27,4 +27,4 @@ struct ip_list
 bool is_cn_ipv4(std::string line);
 std::string get_subnet_mask(ll num);
 std::string get_last_ip(std::string ip, ll num);
-ip_list get_ip(std::string line);
+ip_list get_ip(std::string line);//从delegated-apnic-latest中的CN ipv4一行中返回ip_list
--- getcnIP/源.cpp
+++ getcnIP/源.cpp
@@ -1,5 +1,4 @@
 ﻿#include <iostream>
-
 #include <string>
 #include <vector>
 #include <io.h>
@@ -18,45 +17,36 @@ void getcnip()
 	if (rawdata) {
 		ofstream add, del,ssr;
 		string temp_str;
-		vector<ip_list> ip_mask;
+		queue<ip_list> ip_mask;
 
 		cout << "找到 " + filename << endl;
-		if (_access(string(R"(.\out)").c_str(), 0) == -1)//判断文件夹是否存在
-		{
-			_mkdir(string(R"(.\out)").c_str());//system(("md " + PATH + "\\out").c_str());
-		}	
+		
 		add.open(R"(.\out\add.txt)", ios::trunc);
 		del.open(R"(.\out\del.txt)", ios::trunc);
 		ssr.open(R"(.\out\chn_ip.txt)", ios::trunc);
 
 		cout << "正在分析路由表..." << endl;
+
 		while (getline(rawdata, temp_str, '\n')) {
 			if (is_cn_ipv4(temp_str)) {
-				auto temp_list = get_ip(temp_str);
-				temp_list.cn_ipv4 = temp_str;
-				ip_mask.push_back(temp_list);
+				ip_mask.push(get_ip(temp_str));
 			}
 		}
+
 		cout << "正在生成路由表..." << endl;
 
-		for (auto i = 0; i < ip_mask.size(); ++i) {
-			if (i) {
-				add << endl;
-				del << endl;
-				ssr << endl;
-			}
-			const auto ip = ip_mask.at(i).first_ip.str();
-			const auto mask = ip_mask.at(i).mask;
-			add << "add " + ip + " mask " + mask + " default METRIC default IF default";
-			del << "delete " + ip + " mask " + mask + " default METRIC default IF default";
-			ssr << ip_mask.at(i).first_ip.str() << " " << ip_mask.at(i).last_ip.str();
+		const auto number_of_ip = ip_mask.size();
+		for (;!ip_mask.empty();ip_mask.pop()) {
+			const auto ip = ip_mask.front().first_ip.str();
+			const auto mask = ip_mask.front().mask;
+			add << "add " + ip + " mask " + mask + " default METRIC default IF default" << endl;
+			del << "delete " + ip + " mask " + mask + " default METRIC default IF default" << endl;
+			ssr << ip_mask.front().first_ip.str() << " " << ip_mask.front().last_ip.str() << endl;
 		}
-
-
 		add.close();
 		del.close();
 		ssr.close();
-		cout << "路由表生成成功！共有" << ip_mask.size() << "条。" << endl;
+		cout << "路由表生成成功！共有" << number_of_ip << "条。" << endl;
 	}
 	else {
 		cout << "未找到 " + filename << endl;
@@ -159,8 +149,8 @@ void get_cn_domains()
 			if (temp_str != "")
 			{
 				//取出域名
-				auto pos1 = temp_str.find('/');
-				auto pos2 = temp_str.rfind('/');
+				const auto pos1 = temp_str.find('/');
+				const auto pos2 = temp_str.rfind('/');
 				domains.push("*." + temp_str.substr(pos1+1,pos2-pos1-1));
 			}
 		}
@@ -187,6 +177,11 @@ void get_cn_domains()
 int main() {
 	std::ios::sync_with_stdio(false);
 	setlocale(LC_ALL, "");
+
+	if (_access(string(R"(.\out)").c_str(), 0) == -1)//判断文件夹是否存在
+	{
+		_mkdir(string(R"(.\out)").c_str());//system(("md " + PATH + "\\out").c_str());
+	}
 
 	getcnip();
 
