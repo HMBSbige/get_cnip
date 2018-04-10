bf327232f37b4b001492c5c393c5a5bc93c46b2b
HMBSbige <admin@bige0.com>
2018-04-11 00:01:43 +0800

    add .Net Core

---
+++ getcnIP-dotnetcore/GetCNDomains.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace getcnIP_dotnetcore
+{
+	internal class GetCNDomains
+    {
+	    private const string Path1 = @"accelerated-domains.china.conf";
+	    private const string Path2 = @"whitelist.txt";
+
+	    private string GetDomainFromLine(string str)
+	    {
+		    var sArray = str.Split('/');
+		    return sArray[1];
+	    }
+
+		public List<string> Read()
+	    {
+		    if (!File.Exists(Path1) && !File.Exists(Path2))
+		    {
+			    return null;
+		    }
+
+			var domains=new List<string>();
+
+		    if (File.Exists(Path1))
+		    {
+			    var sr = new StreamReader(Path1, Encoding.UTF8);
+			    string line;
+			    while ((line = sr.ReadLine()) != null)
+			    {
+				    var domain = GetDomainFromLine(line);
+				    if (!string.IsNullOrWhiteSpace(domain))
+					{
+					    domains.Add(domain);
+					}
+					
+			    }
+			}
+			
+		    if (File.Exists(Path2))
+		    {
+			    var sr = new StreamReader(Path2, Encoding.UTF8);
+			    string line;
+			    while ((line = sr.ReadLine()) != null)
+			    {
+				    var domain = line;
+				    if (!string.IsNullOrWhiteSpace(domain))
+				    {
+					    domains.Add(domain);
+				    }
+			    }
+		    }
+
+		    return domains.Count == 0 ? null : domains;
+	    }
+    }
+}
---
+++ getcnIP-dotnetcore/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace getcnIP_dotnetcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	        GetCNDomains g=new GetCNDomains();
+	        var listofdomains = g.Read();
+
+			if (listofdomains == null)
+	        {
+				Console.WriteLine(Directory.GetCurrentDirectory());
+	        }
+	        else
+	        {
+				Console.WriteLine(listofdomains[0]);
+		        Console.WriteLine(listofdomains.Count);
+			}
+
+        }
+    }
+}
