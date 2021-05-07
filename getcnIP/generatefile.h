9f33c5cf0c6cf2d1cf1cab1f83b48f8128ee509c
Bruce Wayne <admin@bige0.com>
2021-05-07 15:03:21 +0800

    cargo init

--- getcnIP/generatefile.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-void getcnip();
-void gfwlist2pac();
-void get_cn_domains();
-void generate_ss_cnip();
-void generate_ss_white();
-void generate_ss_white_r();
-void generate_whitelist_acl();
